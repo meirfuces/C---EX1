02012b2340b782c7833db418f51305b733bbbc5e
GintonD <53877729+GintonD@users.noreply.github.com>
2020-03-24 14:49:05 +0200

    update hpp & start cpp

--- PhoneticFinder.hpp
+++ PhoneticFinder.hpp
@@ -1,7 +1,16 @@
+//
+//#ifndef PHONETICFINDER_HPP_
+//#define PHONETICFINDER_HPP_
+
 #include <string>
-
-
 using namespace std;
-namespace phonetic{
-    string find(string text,string word);
+
+namespace phonetic
+{
+string find(string text, string word);
 }
+char toLowerCase(char c);
+
+
+
+//#endif /* PHONETICFINDER_HPP_ */
